27061a1b98d74992441566fae7b7c40faf3509ae
Rodrigo Almino <rdg.almino@gmail.com>
2022-09-23 21:11:39 -0300

    Conclusion the exercise

--- fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao/main.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao/main.js
@@ -1,9 +1,49 @@
 // ----------------- fatorial
 
-let number = 10;
-let soma = 1;
-for (let index = 1; index <= number; index += 1) {
-  soma *= index;
-}
+// let number = 10;
+// let soma = 1;
+// for (let index = 1; index <= number; index += 1) {
+//   soma *= index;
+// }
 
-console.log(soma);
+// console.log(soma);
+
+// ----------------- inverter
+// let word = "trybe";
+// let invertida = [];
+// for (let index = word.length - 1; index >= 0; index -= 1) {
+//   invertida += word[index];
+// }
+// console.log(invertida);
+
+// ----------------- maior/menor
+// let array = ["java", "javascript", "python", "html", "css"];
+// let maior = [];
+// let menor = array[0];
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length > maior.length) {
+//     maior = array[index];
+//   }
+//   if (array[index].length < menor.length) {
+//     menor = array[index];
+//   }
+// }
+// console.log("O menor nome é " + menor + " e o maior é " + maior);
+
+// ----------------- números primos
+// let number = 50;
+// let primo = 0;
+
+// for (indexA = 1; indexA <= number; indexA += 1) {
+//   let counter = 0;
+//   for (indexB = 1; indexB <= indexA; indexB += 1) {
+//     if (indexA % indexB == 0) {
+//       counter += 1;
+//     }
+//   }
+//   if (counter == 2) {
+//     primo = indexA;
+//   }
+// }
+
+// console.log(primo);
