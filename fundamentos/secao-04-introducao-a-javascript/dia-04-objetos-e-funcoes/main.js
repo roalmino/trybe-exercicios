7fe3c558fa8c513cdce8b2f73adfa0eaf0401ba4
Rodrigo Almino <rdg.almino@gmail.com>
2022-09-26 13:57:26 -0300

    Create exercise objeto

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/main.js
@@ -0,0 +1,9 @@
+//  ------------------------------  exercicios objeto
+let player = {
+  name: "Marta",
+  lastName: "Silva",
+  age: 34,
+  medals: ["golden", "silver"],
+};
+
+console.log(player.medals[1]);
