efa31b9dfd7e23fe78d965886093b2a608a80a69
Rodrigo Almino <rdg.almino@gmail.com>
2022-11-08 18:30:32 -0300

    Gerador de senhas

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,4 +1,7 @@
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
+import './style.css';
+import copy from 'clipboard-copy';
+
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
 
@@ -6,3 +9,8 @@ passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
 });
+
+
+displayPasswordEl.addEventListener('click', () => {
+    copy(displayPasswordEl.innerText)
+})
