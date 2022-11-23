a048d57bdee6ad48f755e7c918224a168f5786e4
Rodrigo Almino <rdg.almino@gmail.com>
2022-11-23 11:55:00 -0300

    exercicio

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/teste/teste/src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rodrigo Almino</h1>
+        <p>Descrição muito boa</p>
+      </div>
+    );
+  }
+}
+
+export default About;
