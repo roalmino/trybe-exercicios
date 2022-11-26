34db03944cc042a9acf7b13189431196455ab63e
Rodrigo Almino <rdg.almino@gmail.com>
2022-11-26 20:09:11 -0300

    exercicios

---
+++ front-end/secao-02-introducao-ao-react/dia-02-componentes-react/fixacao/exercise-image-component/src/App.js
@@ -0,0 +1,13 @@
+import Image from './Image'
+import './App.css';
+import Teste from './teste';
+
+function App() {
+  return (
+    <div>
+           <Image source = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Gatos.jpg/396px-Gatos.jpg" alt ='Gato' />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/secao-02-introducao-ao-react/dia-02-componentes-react/fixacao/exercise-image-component/src/teste.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Teste extends React.Component {
+    render() {
+        console.log('aoi')
+        return <h1>teste</h1>
+    }
+}
+
+export default Teste
